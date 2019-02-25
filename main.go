f961a66d0d383a055f6af285f628911e67615162
Gendos <striker2011@gmail.com>
2019-02-25 02:21:37 +0200

    initial

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/websocket"
+	"io"
+	"log"
+	"net/http"
+)
+
+var hub = []*websocket.Conn{}
+
+func main() {
+	http.Handle("/connws/", websocket.Handler(connWs))
+	log.Fatal(http.ListenAndServe(":555", nil))
+}
+
+func connWs(ws *websocket.Conn) {
+	data := map[string]string{}
+	hub = append(hub, ws)
+
+	for {
+		err := websocket.JSON.Receive(ws, &data)
+		if err != nil && err != io.EOF {
+			log.Println(err)
+			ws.Close()
+			break
+		}
+
+		message := fmt.Sprintf("%s: %s<br>", data["name"], data["text"])
+
+		for i := range hub {
+			err = websocket.Message.Send(hub[i], message)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
